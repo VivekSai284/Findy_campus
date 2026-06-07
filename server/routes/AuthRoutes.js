9530c64a5dbc0cf1a02803da2cef016b4a7ec275
Vivek Sai <viveksaikonada2006@gmail.com>
2026-06-07 15:45:15 +0530

    Initial commit

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const User = require('../models/User');z
+const User = require('../models/User');
 const authMiddleware = require('../middleware/authMiddleware')
 const { findOne } = require('../models/Item');
 
